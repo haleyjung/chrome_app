743383ea1a9074354e3c2be9d564de37b5ec86ae
haleyjung <73789849+haleyjung@users.noreply.github.com>
2020-11-19 18:33:53 -0500

    Updated greetings

--- js/greeting.js
+++ js/greeting.js
@@ -1,22 +1,40 @@
-const form = document.querySelector(".js-form"),
-    input = form.querySelector("input"),
-    greeting = document.querySelector(".js-greetings");
+const nameContainer = document.querySelector(".js-name");
 
-const USER_LS = "currentUser",
-    SHOWING_CN = "showing";
+function paintName(name) {
+    nameContainer.innerHTML = "";
+    const title = document.createElement("span");
+    title.className = "name__text";
+    title.innerHTML = `Hello ${name}`;
+    nameContainer.appendChild(title);
+}
 
-function paintGreeting(text) {
-    form.classList.remove(SHOWING_CN);
-    greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${text}`;
+function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const input = form.querySelector("input");
+    const value = input.value;
+    localStorage.setItem("username", value);
+    paintName(value);
+}
+
+function paintInput() {
+    const input = document.createElement("input");
+  input.placeholder = "Type your name here";
+  input.type = "text";
+  input.className = "name__input";
+  const form = document.createElement("form");
+  form.addEventListener("submit", handleSubmit);
+  form.appendChild(input);
+  nameContainer.appendChild(form);
 }
 
 function loadName(){
-    const currentUser = localStorage.getItem(USER_LS);
-    if(currentUser === null) {
-    } else {
-        paintGreeting(currentUser);
-    }
+    const name = localStorage.getItem("username");
+  if (name === null) {
+    paintInput();
+  } else {
+    paintName(name);
+  }
 }
 
 function init() {
