73390ac859f97a33be9688d09563777b72a6f7a8
haleyjung <73789849+haleyjung@users.noreply.github.com>
2020-11-19 18:18:02 -0500

    Updated weather and geo location

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,22 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "1706261ffc689a5d0ffe73f0d2f8a925";
 const COORDS = "coords";
 
+function getWeather(lat, lng){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+        return response.json();
+    })
+        .then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
+
 function saveCoords(coordsObj){
-    localStorage.setItem(COORDS, JSON.stringify(coordsOjb));
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
 function handleGeoSuccess(position){
@@ -13,6 +27,7 @@ function handleGeoSuccess(position){
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -28,7 +43,8 @@ function loadCoords(){
     if(loadedCoords === null){
         askForCoords();
     } else {
-        // getWeather
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
 }
 
